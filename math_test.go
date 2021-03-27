92766d6bf5f2581ba3a86e77bb161570bb544640
JONATHAN MARQUES DA COSTA <jonathanmarquesdacosta@gmail.com>
2021-03-27 11:44:20 -0300

    First Commit

---
+++ math_test.go
@@ -0,0 +1,13 @@
+package main
+
+import "testing"
+
+func TesteSoma(t *testing.T) {
+
+	total := soma(11, 15)
+
+	if total != 31 {
+		t.Errorf("resultado da soma é inválido: Resultado %d. Esperado: %d", total, 30)
+	}
+
+}
